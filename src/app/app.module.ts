8dc1d56c2aa2e37df55f541094236d7f68affee8
Junus Ergin <junus@developerakamie.com>
2020-08-04 17:05:13 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { LandingComponent } from './landing/landing.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    LandingComponent
   ],
   imports: [
     BrowserModule,
