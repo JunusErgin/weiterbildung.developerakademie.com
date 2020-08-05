7355f0ecbe5969c00f5d8e05bbe936dc93bee16c
Junus Ergin <junus@developerakamie.com>
2020-08-05 15:35:26 +0200

    first draft

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LandingComponent } from './landing/landing.component';
+import { ApplyComponent } from './apply/apply.component';
+import { ThankYouPageComponent } from './thank-you-page/thank-you-page.component';
 
 
 const routes: Routes = [{
   path: '',
   component: LandingComponent,
-},];
+}, {
+  path: 'termin',
+  component: ApplyComponent,
+}, {
+  path: 'bewerbung-eingegangen',
+  component: ThankYouPageComponent,
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
 import { LandingComponent } from './landing/landing.component';
+import { FooterComponent } from './footer/footer.component';
+import { ApplyComponent } from './apply/apply.component';
+import { ThankYouPageComponent } from './thank-you-page/thank-you-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
-    LandingComponent
+    LandingComponent,
+    FooterComponent,
+    ApplyComponent,
+    ThankYouPageComponent
   ],
   imports: [
     BrowserModule,
